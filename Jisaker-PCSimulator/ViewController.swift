05fac89baf143bceebe7e21d08dd87df5450c315
HinaKinoooooN <hinata6029@gmail.com>
2024-11-21 05:23:25 +0900

    Initial Commit

---
+++ Jisaker-PCSimulator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jisaker-PCSimulator
+//
+//  Created by 木下陽汰 on 2024/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
